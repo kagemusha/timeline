f80ecf7a966aaae0d1c1d4ce5f4d01174911b569
kagemusha <mmadrid99@gmail.com>
2016-03-28 21:59:52 -0400

    join game channel
    uses ember-phoenix-channel

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,12 +3,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   gameCode: null,
+  channelService: Ember.inject.service(),
 
   actions: {
     newGame() {
       const code = this.controllerFor('index').get('newGameCode');
       const game = this.store.createRecord('game', {code: code});
       game.save().then((game) => {
+        const channelService = this.get('channelService');
+        channelService.joinChannel(`game:${game.get('id')}`);
         this.transitionTo('game', game);
       });
     },
