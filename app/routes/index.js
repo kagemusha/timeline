502c9aae2a0fba7f5a98c873dd84af62330d0e04
kagemusha <mmadrid99@gmail.com>
2016-05-08 09:39:03 -0400

    image-timeline component

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 const { inject } = Ember;
 
+const imagesSrc = [
+  'http://www.china-mike.com/wp-content/uploads/2011/03/terracotta-army-warriors-xian.png',
+  'http://media.washtimes.com.s3.amazonaws.com/media/image/2015/03/18/caesar_2.jpg',
+  'http://images.fineartamerica.com/images-medium-large/8-christopher-columbus-granger.jpg',
+  'http://www.nasa.gov/images/content/2333main_MM_Image_Feature_19_rs4.jpg',
+];
+
 export default Ember.Route.extend({
   gameCode: null,
   channelService: inject.service(),
@@ -26,6 +33,10 @@ export default Ember.Route.extend({
     this.transitionTo('game');
   },
 
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('imagesSrc', imagesSrc);
+  },
   actions: {
 
     createGame() {
