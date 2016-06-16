a8082fc9769ac01b2f32052a30f93c3d2df04913
kagemusha <mmadrid99@gmail.com>
2016-06-16 12:23:57 -0400

    display month when present

--- app/models/card.js
+++ app/models/card.js
@@ -1,9 +1,14 @@
 import DS from 'ember-data';
 
 const { attr } = DS;
-
+const months = ["Jan","Feb", "Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
 export default DS.Model.extend({
   game: DS.belongsTo('game'),
   event: attr(),
-  year: attr()
+  year: attr(),
+  month: attr(),
+  timeLabel: Ember.computed('year', 'month', function(){
+    const month = this.get('month');
+    return `${month ? months[month] + " ":""}${this.get('year')}`
+  })
 });
